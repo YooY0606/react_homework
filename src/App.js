3f2b404d800e8e30649a5074db335266e785b9f3
YooY0606 <B0829021@cgu.edu.tw>
2021-05-03 21:15:10 +0800

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './login'
+
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+          {Login()}
+        </div>
+    </div>
+  );
+}
+
+export default App;
